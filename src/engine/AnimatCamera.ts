d570cc3ccb4c66d6b7526075d01a5bcc72240b6c
wangpengfei <2601339907@qq.com>
2022-01-13 18:08:36 +0800

    add animatCamera

--- src/engine/AnimatCamera.ts
+++ src/engine/AnimatCamera.ts
@@ -2,26 +2,72 @@ import {PerspectiveCamera, Vector3} from "three";
 import AnimatState3 from "./animatState/AnimatState3";
 
 class AnimatCamera extends PerspectiveCamera {
-    public animatState: {
-        position: AnimatState3;
-        target: AnimatState3;
+    private _inAnimation: boolean = false;
+    public get inAnimation() {
+        return this._inAnimation;
+    }
+    private startTime = 0;
+    private animatState: {
+        position: {
+            active: boolean;
+            value: AnimatState3;
+        };
+        target: {
+            active: boolean;
+            value: AnimatState3;
+        };
     };
-
     public constructor(fov?: number, aspect?: number, near?: number, far?: number) {
         super(fov, aspect, near, far);
 
         this.animatState = {
-            position: new AnimatState3(new Vector3(0, 0, 100)),
-            target: new AnimatState3(new Vector3())
+            position: {
+                active: false,
+                value: new AnimatState3(this.position)
+            },
+            target: {
+                active: false,
+                value: new AnimatState3(new Vector3())
+            }
         };
     }
 
-    public updatePosition() {
-        this.position.copy(this.animatState.position.state);
+    private updatePosition() {
+        this.position.copy(this.animatState.position.value.state);
     }
 
-    public updateTarget() {
-        this.lookAt(this.animatState.target.state);
+    private updateTarget() {
+        this.lookAt(this.animatState.target.value.state);
+    }
+
+    public setAnimatPosition(end: Vector3) {
+        this.animatState.position.active = true;
+        this.animatState.position.value.end.copy(end);
+    }
+
+    public setAnimatTarget(end: Vector3) {
+        this.animatState.target.active = true;
+        this.animatState.target.value.end.copy(end);
+    }
+
+    public startAnimation(startTime: number) {
+        this.animatState.position.value.start.copy(this.position);
+        this._inAnimation = true;
+    }
+
+    public endAnimation() {
+        this._inAnimation = false;
+    }
+
+    public update(time: number) {
+        if (this.inAnimation) {
+            const t = time - this.startTime;
+            this.animatState.position.value.update(t);
+            this.animatState.target.value.update(t);
+
+            this.updatePosition();
+            this.updateTarget();
+        }
     }
 }
 
