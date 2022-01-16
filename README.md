5747a55dfa07c53327ec27f6852747b0ddec7b5c
Wang Pengfei <13671619364@sina.cn>
2022-01-16 11:07:23 +0800

    add deploy workflow

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-threejs-case project.
+## threejs-case
+
+使用 threejs 写的小玩意。
+
+## case
+
+建议使用谷歌浏览器查看。
+
+1. gallery: [https://pengfeiw.github.io/threejs-case/gallery](https://pengfeiw.github.io/threejs-case/gallery)
--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const withPlugins = require("next-compose-plugins");
 const {merge} = require("webpack-merge");
 const webpackconfig = require("./webpack.config");
 
+const IsDevelopment = process.env.NODE_ENV === "development";
+
 const nextconfig = {
     reactStrictMode: true,
+    assetPrefix: IsDevelopment ? "" : "/threejs-case",
 };
 
 module.exports = withPlugins(
